cdecf9d48776bd6b391cca1f2d8c1684aa7a63d8
harryrollings <harryrollings@icloud.com>
2023-08-17 21:03:49 +0100

    call split out shift actions

--- client/src/Pages/Workspace/admin/shift.card.tsx
+++ client/src/Pages/Workspace/admin/shift.card.tsx
@@ -1,26 +1,25 @@
 import React, { useState } from "react";
-import { GET_SHIFTS_FOR_DEPARTMENT_QUERY, ParsedShiftResponse } from "./workspace.department.page";
+import { gql, useMutation } from "@apollo/client";
 import { Workspace } from "../../../Types/Workspace";
 import WorkspaceUserSelectorModal from "../../../Components/workspace-user-selector-modal";
 import ShiftStateText from "./components/shift.statetext";
-import { gql, useMutation, useQuery } from "@apollo/client";
+import { ParsedShiftResponse } from "./workspace.department.page";
 
 const ASSIGN_SHIFT_MUTATION = gql`
-  mutation AssignShift($shiftid: UUID!, $accountId: String!) {
-    actionShift(request: {action: ASSIGN, state: ASSIGNED, shiftId: $shiftid, accountId: $accountId}) {
-        id
-      }
+  mutation AssignShift($request: AssignDataInput!) {
+    assignShift(request: $request) {
+      id
+    }
   }
 `;
 
 const UNASSIGNED_SHIFT_MUTATION = gql`
-  mutation UnassignShift($shiftid: UUID!, $accountId: String!) {
-    actionShift(request: {action: UNASSIGN, state: UNASSIGNED_AND_HIDDEN, shiftId: $shiftid, accountId: $accountId}) {
-        id
-      }
+  mutation UnassignShift($request: ShiftIdRequestInput!) {
+    unassignShift(request: $request) {
+      id
+    }
   }
 `;
-
 
 interface CardProps {
   shift: ParsedShiftResponse;
@@ -29,95 +28,91 @@ interface CardProps {
 }
 
 const ShiftCard: React.FC<CardProps> = ({ shift, workspace, refreshDepartment }) => {
-    const [showUserSelectorModal, setShowUserSelectorModal] = useState<boolean>(false);
+  const [showUserSelectorModal, setShowUserSelectorModal] = useState<boolean>(false);
 
-    const handleCloseModal = () => {
-        setShowUserSelectorModal(false);
-    };
+  const handleCloseModal = () => {
+    setShowUserSelectorModal(false);
+  };
 
-    const [assignShiftMutation] = useMutation(ASSIGN_SHIFT_MUTATION, {
-        context: {
-            headers: {
-              WorkspaceId: workspace.id,
-            },
-          },
-    });
+  const [assignShiftMutation] = useMutation(ASSIGN_SHIFT_MUTATION, {
+    context: {
+      headers: {
+        WorkspaceId: workspace.id,
+      },
+    },
+  });
 
-    const [unassignShiftMutation] = useMutation(UNASSIGNED_SHIFT_MUTATION, {
-        context: {
-            headers: {
-              WorkspaceId: workspace.id,
-            },
-          },
-    });
+  const [unassignShiftMutation] = useMutation(UNASSIGNED_SHIFT_MUTATION, {
+    context: {
+      headers: {
+        WorkspaceId: workspace.id,
+      },
+    },
+  });
 
+  const handleSubmitModal = async (accountId: string) => {
+    try {
+      // Call the mutation to assign the user to the shift
+      await assignShiftMutation({
+        variables: { request: { shiftId: shift.id, accountId } },
+      });
 
-    const handleSubmitModal = async (accountId: string) => {
-        try {
-            // Call the mutation to assign the user to the shift
-            await assignShiftMutation({
-                variables: { shiftid: shift.id, accountId },
-            });
+      // Close the modal after successful assignment (optional)
+      handleCloseModal();
 
-            // Close the modal after successful assignment (optional)
-            handleCloseModal();
+      console.log("going to refresh...");
+      // Refetch the shifts data to get the latest changes
+      refreshDepartment();
+    } catch (error) {
+      // Handle any errors that occur during the mutation
+      console.error('Error assigning user to shift:', error);
+      // Optionally, show an error message to the user
+    }
+  };
 
-            console.log("going to refresh...")
-            // Refetch the shifts data to get the latest changes
-            refreshDepartment();
-        } catch (error) {
-            // Handle any errors that occur during the mutation
-            console.error('Error assigning user to shift:', error);
-            // Optionally, show an error message to the user
-        }
-    };
+  const handleUnassignedShift = async () => {
+    try {
+      // Call the mutation to unassign the user from the shift
+      await unassignShiftMutation({
+        variables: { request: { shiftId: shift.id } },
+      });
 
-
-    const handleUnassignedShift = async (accountId: string) => {
-        try {
-            // Call the mutation to assign the user to the shift
-            await unassignShiftMutation({
-                variables: { shiftid: shift.id, accountId },
-            });
-
-            // Refetch the shifts data to get the latest changes
-            console.log("going to refresh...")
-            refreshDepartment();
-        } catch (error) {
-            // Handle any errors that occur during the mutation
-            console.error('Error assigning user to shift:', error);
-            // Optionally, show an error message to the user
-        }
-    };
+      // Refetch the shifts data to get the latest changes
+      console.log("going to refresh...");
+      refreshDepartment();
+    } catch (error) {
+      // Handle any errors that occur during the mutation
+      console.error('Error unassigning user from shift:', error);
+      // Optionally, show an error message to the user
+    }
+  };
 
   const handleAssignUser = () => {
-    // Handle assign user logic
-    console.log("Assigning user to shift:", shift.id);
-    setShowUserSelectorModal(true)
+    setShowUserSelectorModal(true);
   };
 
   return (
     <div>
       <div className="bg-white shadow-md rounded-md p-4 mb-4 flex items-center justify-between">
         <div>
-          <p className="mb-2">
-            Start Time:{" "}
-            {shift.shiftStartTime.toLocaleDateString() +
-              " " +
-              shift.shiftStartTime.toLocaleTimeString()}
-          </p>
-          <p className="mb-2">
-            End Time:{" "}
-            {shift.shiftEndTime.toLocaleDateString() +
-              " " +
-              shift.shiftEndTime.toLocaleTimeString()}
-          </p>
-          <ShiftStateText shift={shift}></ShiftStateText>
-        </div>
+            <p className="mb-2">
+              Start Time:{" "}
+              {shift.shiftStartTime.toLocaleDateString() +
+                " " +
+                shift.shiftStartTime.toLocaleTimeString()}
+            </p>
+            <p className="mb-2">
+              End Time:{" "}
+              {shift.shiftEndTime.toLocaleDateString() +
+                " " +
+                shift.shiftEndTime.toLocaleTimeString()}
+            </p>
+            <ShiftStateText shift={shift}></ShiftStateText>
+          </div>
         {shift.assignedUser ? (
           <button
             className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 focus:outline-none"
-            onClick={() => handleUnassignedShift(shift.assignedUser!.accountId)}
+            onClick={handleUnassignedShift}
           >
             Unassign User
           </button>
@@ -133,13 +128,12 @@ const ShiftCard: React.FC<CardProps> = ({ shift, workspace, refreshDepartment })
       {showUserSelectorModal && (
         <WorkspaceUserSelectorModal
           users={workspace.users}
-          onClose={handleCloseModal} // Pass a callback to handle modal close
+          onClose={handleCloseModal}
           onSubmit={handleSubmitModal}
         />
       )}
     </div>
   );
-  
 };
 
 export default ShiftCard;
